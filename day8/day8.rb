40b7ad915693e06e5d91bffd239ab101a5253410
Ting Ting Chen <tin2294@gmail.com>
2021-12-17 02:11:01 +0100

    challenge2 day8

--- day8/day8.rb
+++ day8/day8.rb
@@ -1,31 +1,27 @@
 test = "
-be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb |
-fdgacbe cefdb cefbgd gcbe
-edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec |
-fcgedb cgb dgebacf gc
-fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef |
-cg cg fdcagb cbg
-fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega |
-efabcd cedba gadfec cb
-aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga |
-gecf egdcabf bgf bfgea
-fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf |
-gebdcfa ecba ca fadegcb
-dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf |
-cefg dcbef fcge gbcadfe
-bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd |
-ed bcgafe cdgba cbgef
-egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg |
-gbdfcae bgc cg cgb
-gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |
-fgae cfgab fg bagce
+acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf
 "
 
 def convert(test)
-  result = test.split("|")
-  puts result[0]
+  delimiters = ["\n"]
+  result = test.split(Regexp.union(delimiters))
+  array = []
+  result.each do |words|
+    array << words.split("|")
+  end
+  array.each_with_index { |group, index| array.delete_at(index) if group == [] }
+  final_array = array.map { |word| word[1].split(" ") }
+  count = 0
+  final_array.each do |wordf|
+    wordf.each do |word1|
+      if word1.length == 2 || word1.length == 4 || word1.length == 3 || word1.length == 7
+        count += 1
+      end
+    end
+  end
+  return count
 end
 
 # with regex create two match groups, one of them with the \n and the other one without it, which would be the output values.
 # count the amount of characters in group 2 and array them then sum and that's it for challenge 1
-convert(test)
+p convert(test)
---
+++ day8/day8challenge2.rb
@@ -0,0 +1,76 @@
+test = "
+acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf
+"
+
+def output(test)
+  delimiters = ["\n"]
+  result = test.split(Regexp.union(delimiters))
+  array = []
+  result.each do |words|
+    array << words.split("|")
+  end
+  array.each_with_index { |group, index| array.delete_at(index) if group == [] }
+  array.map { |word| word[1].split(" ") }
+end
+
+def input(test)
+  delimiters = ["\n"]
+  result = test.split(Regexp.union(delimiters))
+  array = []
+  result.each do |words|
+    array << words.split("|")
+  end
+  array.each_with_index { |group, index| array.delete_at(index) if group == [] }
+  array.map { |word| word[0].split(" ") }
+end
+
+def includes(big, sub)
+  sub = sub.split("")
+  big = big.split("")
+  final = sub.map { |letter| big.include?(letter) }
+  return final.all?
+end
+
+# big = "cdfgeb"
+# small = "cdfbe"
+# p includes(big, small)
+
+def get_numbers(array)
+  hash_final = Hash.new(0)
+  array.each do |digit|
+    if digit.length == 2
+      hash_final[1] = digit
+    elsif digit.length == 4
+      hash_final[4] = digit
+    elsif digit.length == 7
+      hash_final[8] = digit
+    elsif digit.length == 3
+      hash_final[7] = digit
+    # elsif digit.length == 6
+    #   if includes(digit, hash_final[4][0])
+    #     hash_final[9] = digit
+    #   elsif includes(digit, hash_final[1][0])
+    #     hash_final[0] = digit
+    #   else
+    #     hash_final[6] = digit
+    #   end
+    # elsif digit.length == 5
+    #   if includes(digit, hash_final[1][0])
+    #     hash_final[3] = digit
+    #   elsif includes(hash_final[6][0], digit)
+    #     hash_final[5] = digit
+    #   else
+    #     hash_final[2] = digit
+    #   end
+    end
+  end
+  return hash_final
+end
+
+output = output(test)[0]
+input =  input(test)[0]
+
+p output
+
+p get_numbers(output)
+# p get_numbers(input)
